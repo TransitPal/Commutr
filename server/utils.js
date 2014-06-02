0f8961007588391bb193aec407f83860c01f87f7
Andrew Krause <a.c.krause@gmail.com>
2014-06-02 09:57:41 -0700

    Adds utility function for constructing server responses of the form {time: nextRequestTime, route: currentRoute}

--- server/utils.js
+++ server/utils.js
@@ -1,3 +1,5 @@
+var maps = require('./mapUtils.js');
+
 var getNextTime = function(time) {
   var now = new Date();
   var testTime = new Date();
@@ -12,6 +14,57 @@ var getNextTime = function(time) {
   }
 };
 
+var getNextServerRequest = function(user, res, resCode) {
+  resCode = resCode || 200;
+
+  // get user routine
+  var routine = user.routine;
+
+  // get next work time and next home time
+  var nextWorkTime = getNextTime(routine.workTime);
+  var nextHomeTime = getNextTime(routine.homeTime);
+  var now = new Date();
+
+  // determine which route
+  var origin, destination;
+  var nextRequest;
+
+  if (now < nextHomeTime && now < nextWorkTime) {
+    // user is at work, wants to go home
+    origin = user.workLocation;
+    destination = user.homeLocation;
+    // next request will be at home, for work
+    nextRequest = nextWorkTime;
+  } else {
+    // user is at home, wants to go to work
+    origin = user.homeLocation;
+    destination = user.workLocation;
+    // next request will be at work, for home
+    nextRequest = nextHomeTime;
+  }
+
+  // get route
+  maps.getDirections(origin, destination)
+  .then(function(route) {
+
+    // calculate transit time in miliseconds
+    var transitTime = maps.getTransitTime(route) * 1000;
+
+    // calculate next request time
+    console.log(nextRequest);
+    nextRequest = nextRequest.getTime();
+    nextRequest -= transitTime;
+    nextRequest -= 30*60*1000; // 30 min buffer
+
+    res.send(resCode, {time: nextRequest, route: route});
+  })
+  .catch(function(err) {
+    console.error('ERROR:', err);
+    res.send(500, err);
+  });
+};
+
 module.exports = {
-  getNextTime: getNextTime
+  getNextTime: getNextTime,
+  getNextServerRequest: getNextServerRequest
 };
