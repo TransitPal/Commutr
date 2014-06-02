8a07afb6e445365593b4fa3275d95eead6cd6f1a
Andrew Krause <a.c.krause@gmail.com>
2014-06-02 09:58:57 -0700

    Refactors add/update user route and routes route to use server response utility function

--- server/routeHelpers.js
+++ server/routeHelpers.js
@@ -2,6 +2,7 @@ var models = require('../db/models/user');
 var db = require('../db/dbHelpers');
 var User = require('../db/models/user').User;
 var maps = require('./mapUtils');
+var utils = require('./utils.js');
 var fs = require('fs');
 
 exports.placeholder = function(req, res){
@@ -18,21 +19,9 @@ exports.serveIndex = function(req, res){
 
 exports.getRoutes = function(req, res) {
   // returns promised user homeLocation and workLocation based on input email
-  db.getUserLocations(req.query.email)
-  .then(function(userLocations){
-    if (!userLocations) {
-      console.log('User not found');
-      return res.send(400);
-    }
-    // returns promised directions object for userLocations
-    maps.getDirections(userLocations.homeLocation, userLocations.workLocation)
-    .then (function(route) {
-      // getTransitTime returns total transit time for given route
-      res.send(200, {time: maps.getTransitTime(route), route: route});
-    })
-    .catch(function(err) {
-      res.send(500, err);
-    });
+  db.getUser(req.query.email)
+  .then(function(user){
+    utils.getNextServerRequest(user, res);
   }, function(err) {
     res.send(500, err);
   });
@@ -85,14 +74,11 @@ exports.saveUser = function(req, res){
             res.send(500,err);
             return console.error('Error:', err);
           }
-          // Responds to client with transit time
-          maps.getDirections(user.homeLocation, user.workLocation)
-          .then(function(route) {
-            res.send(201, {time: maps.getTransitTime(route)});
-          })
-          .catch(function(err){
-            console.error('Error:',err);
-          });
+
+          console.log(user);
+          // Responds to client with time of next request
+          utils.getNextServerRequest(user, res, 201);
+
           console.log('User settings saved', user);
         });
 
