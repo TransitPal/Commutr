e6f4a29e047399c108a62669943cc0f912552067
Andrew Krause <a.c.krause@gmail.com>
2014-06-02 07:52:42 -0700

    Adds helper function for retrieving user from database

--- db/dbHelpers.js
+++ db/dbHelpers.js
@@ -3,3 +3,7 @@ var db = require('mongoose');
 exports.getUserLocations = function(email){
   return db.models.User.findOne({email:email}).select('homeLocation workLocation').exec();
 };
+
+exports.getUser = function(email) {
+  return db.models.User.findOne({email: email}).exec();
+};
